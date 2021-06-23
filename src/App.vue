5548271fc281f716689cfbdb3a75c51e57accfe7
alexis sae an <80662164+apprehensive-fig@users.noreply.github.com>
2021-06-23 20:36:41 +0900

    added playlists

--- src/App.vue
+++ src/App.vue
@@ -6,14 +6,20 @@
       <div class="p-6">
       <img src="spotifyLogo.png" class="h-10">
       </div>
-      <div class="mx-2">
+      <div class="mx-2 mb-5">
         <button v-for="page in pages" v-bind:key="page" @click="setID = page.id" :class="`w-full text-xs font-semibold rounded px-2 py-2 flex items-center justify-start ${setID === page.id ? 'bg-light text-white' : 'text-lightest'}`">
-          <i class="material-icons"> {{page.icon}} </i>
+          <i class="material-icons ml-3"> {{page.icon}} </i>
           <p> {{ page.name }} </p>
         </button>
       </div>
+    <div class="mx-5">
+      <h1 class="mb-3 text-xs text-lightest tracking-widest uppercase">Playlists</h1>
+        <button class="flex items-center justify-start opacity-75 hover:opacity-100">
+          <img src="add.png" class="h-8 w-8 mr-3" style="filter: brightness(0) invert(1);"/>
+          <p class="text-sm text-white font-semibold">Create Playlist</p>
+        </button>
+      </div>
     </div>
-
     <div class="w-full h-full relative">
 
       <div class="w-full sticky top-0 p-2">
@@ -33,9 +39,9 @@ export default {
   data: function() {
     return {
       pages: [
-        {id: 'home', name: 'Home', icon: 'home'},
-        {id: 'search', name: 'Search', icon: 'search'},
-        {id: 'library', name: 'Your Library', icon: 'library'}
+        {id: 'home', name: 'Home', icon: ''},
+        {id: 'search', name: 'Search', icon: ''},
+        {id: 'library', name: 'Your Library', icon: ''}
       ],
       setID: 'home'
     }
